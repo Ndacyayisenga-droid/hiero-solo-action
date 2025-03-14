a9e8856467d257f0f0622f78bd5050b78fe6ddcd
Tayebwa Noah <ndacyayinoah@gmail.com>
2025-03-14 16:51:18 +0300

    Update extractAccountAsJson.py

--- extractAccountAsJson.py
+++ extractAccountAsJson.py
@@ -1,10 +1,9 @@
-import re
 import sys
-import os
-input_text = sys.stdin.read()
-json_regex = r'\{\s*\"accountId\":\s*\".*?\",\s*\"publicKey\":\s*\".*?\",\s*\"balance\":\s*\d+\s*\}'
-json_match = re.search(json_regex, input_text)
 
-if json_match:
-    json_block = json_match.group(0)
-    print(json_block)
+input_text = sys.stdin.read()
+account_id = re.search(r'Account ID:\s*(.*)', input_text).group(1)
+public_key = re.search(r'Public Key:\s*(.*)', input_text).group(1)
+balance = re.search(r'Balance:\s*(\d+)', input_text).group(1)
+
+json_block = f'{{"accountId": "{account_id}", "publicKey": "{public_key}", "balance": {balance}}}'
+print(json_block)
